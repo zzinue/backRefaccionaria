03debe22cc94763169efdbe4d33eac7bcf0c1dc1
zzinue <zzinue@gmail.com>
2022-06-02 21:32:23 -0500

    app

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const apiRouter = require('./src/routes');
 const { logErrors, errorHandler } = require('./src/middlewares/errorHandler');
 const db = require('./src/lib/db');
 const config = require('./src/lib/config');
+const { response } = require('express');
 const port = config.app.port;
 
 app.use(express.json()); //middleware
@@ -12,6 +13,11 @@ apiRouter(app);
 
 app.use(logErrors);
 app.use(errorHandler);
+app.get('/', (request, response) => {
+    return response.json({
+        message: 'Todo cool',
+    })
+})
 
 app.listen(port, () => {
     console.log(`Welcome to ${config.app.name} app, now listening on ${port}`);
